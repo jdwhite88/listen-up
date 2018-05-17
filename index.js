3f5e8cfa9d7d33f74d33166570cb83156b624c36
jdwhite88 <white570@purdue.edu>
2018-05-17 08:50:22 -0400

    Delete item from list

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ const songArray = [];
 // Add to both internal list (array) and external list (<ul>)
 const addMusicToList = function(ev) {
     ev.preventDefault();
-    const song = form.musicTitle.value;
-    songArray.push(song);
     songList.appendChild(buildListItem());
     form.reset();
 }
@@ -16,8 +14,25 @@ function buildListItem() {
     const song = form.musicTitle.value;
     songArray.push(song);
     const listItem = document.createElement('li');
+    listItem.id = song;
     listItem.textContent = song;
+    const deleteButton = document.createElement('button');
+    const buttonText = document.createTextNode("Delete");
+    deleteButton.type = 'button';
+    deleteButton.style.display = 'inline-block';
+    deleteButton.addEventListener('click', deleteListItem);
+    deleteButton.appendChild(buttonText);
+    listItem.appendChild(deleteButton);
     return listItem;
 }
 
 form.addEventListener('submit', addMusicToList);
+
+function deleteListItem(ev) {
+    const listItem = ev.target.parentElement;
+    const songIndex = songArray.indexOf(listItem.id);
+    if (songIndex >= 0) {
+        songArray.splice(songIndex, 1);
+    }
+    songList.removeChild(listItem);
+}
