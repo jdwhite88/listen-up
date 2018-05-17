b5272564e5eace963504d7750479931cd3936b8a
jdwhite88 <white570@purdue.edu>
2018-05-16 20:34:42 -0400

    Add name to displayed list

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const addButton = document.querySelector('button.newMusic');
 const form = document.querySelector('form.newMusic');
+const songList = document.querySelector('#musicList');
 const songArray = [];
+
+// Add to both internal list (array) and external list (<ul>)
 const addMusicToList = function(ev) {
     ev.preventDefault();
-
     const song = form.musicTitle.value;
     songArray.push(song);
+    songList.appendChild(buildListItem());
     form.reset();
+}
+
+function buildListItem() {
+    const song = form.musicTitle.value;
+    songArray.push(song);
+    const listItem = document.createElement('li');
+    listItem.textContent = song;
+    return listItem;
 }
 
 form.addEventListener('submit', addMusicToList);
