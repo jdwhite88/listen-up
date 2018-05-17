6162941c3362d049f06f89b84dfc6aedcc508a3e
jdwhite88 <white570@purdue.edu>
2018-05-17 15:27:16 -0400

    Refactor code into object & insert top of list

--- index.js
+++ index.js
@@ -1,38 +1,60 @@
-const addButton = document.querySelector('#musicForm button');
-const form = document.querySelector('#musicForm');
-const songList = document.querySelector('#musicList');
-const songArray = [];
+const app = {
 
-// Add to both internal list (array) and external list (<ul>)
-const addMusicToList = function(ev) {
-    ev.preventDefault();
-    songList.appendChild(buildListItem());
-    form.reset();
+    init(selectors) {
+        this.max = 0;
+        this.songArray = [];
+        this.songList = document.querySelector(selectors.listSelector);
+        document
+            .querySelector(selectors.formSelector)
+            .addEventListener('submit', (ev) => {
+                ev.preventDefault();
+                this.addMusicToList(ev);
+            });
+    },
+
+    addMusicToList(ev) {
+        ev.preventDefault();
+        const form = ev.target;
+
+        const listItem = this.buildListItem(ev);
+        this.songList.insertBefore(listItem, this.songList.firstChild);
+        form.reset();
+    },
+
+    buildListItem(ev) {
+        const form = ev.target;
+        const song = {
+            id: ++this.max,
+            name: form.musicName.value,
+        }
+        this.songArray.push(song);
+        const listItem = document.createElement('li');
+        listItem.dataset.id = song.id;
+        listItem.dataset.name = song.name;
+        listItem.textContent = song.name;
+        const deleteButton = document.createElement('button');
+        const buttonText = document.createTextNode('Delete');
+        deleteButton.type = 'button';
+        deleteButton.style.display = 'inline-block';
+        deleteButton.addEventListener('click', (deleteEv) => {
+            this.deleteListItem(deleteEv);
+        });
+        deleteButton.appendChild(buttonText);
+        listItem.appendChild(deleteButton);
+        return listItem;
+    },
+
+    deleteListItem(ev) {
+        const listItem = ev.target.parentElement;
+        const songIndex = this.songArray.indexOf(listItem.dataset.name);
+        if (songIndex >= 0) {
+            this.songArray.splice(songIndex, 1);
+        }
+        this.songList.removeChild(listItem);
+    },
 }
 
-function buildListItem() {
-    const song = form.musicTitle.value;
-    songArray.push(song);
-    const listItem = document.createElement('li');
-    listItem.id = song;
-    listItem.textContent = song;
-    const deleteButton = document.createElement('button');
-    const buttonText = document.createTextNode("Delete");
-    deleteButton.type = 'button';
-    deleteButton.style.display = 'inline-block';
-    deleteButton.addEventListener('click', deleteListItem);
-    deleteButton.appendChild(buttonText);
-    listItem.appendChild(deleteButton);
-    return listItem;
-}
-
-form.addEventListener('submit', addMusicToList);
-
-function deleteListItem(ev) {
-    const listItem = ev.target.parentElement;
-    const songIndex = songArray.indexOf(listItem.id);
-    if (songIndex >= 0) {
-        songArray.splice(songIndex, 1);
-    }
-    songList.removeChild(listItem);
-}
+app.init({
+    formSelector: '#musicForm',
+    listSelector: '#musicList',
+});
